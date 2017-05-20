916672a87d02e4471a2c9264b2ce155442787560
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-05-20 17:36:57 +0100

    feature(admin manage roles): admin able to manage role[finish/#144439479]

--- server/controller/RolesController.js
+++ server/controller/RolesController.js
@@ -28,7 +28,84 @@ const RoleController = {
           }));
   },
 
- 
+  /**
+    * Get all roles
+    * Route: GET: /roles/
+    * @param {Object} req request object
+    * @param {Object} res response object
+    * @returns {void} no returns
+    */
+  getAllRoles(req, res) {
+    db.Roles
+      .findAll()
+      .then((roles) => {
+        res.status(200)
+        .send({
+          message: 'You have successfully retrieved all roles',
+          roles
+        });
+      });
+  },
+  /**
+    * Update roles
+    * Route: PUT: /roles/:id
+    * @param {Object} req request object
+    * @param {Object} res response object
+    * @returns {void} no returns
+    */
+  updateRole(req, res) {
+    req.roleInstance.update(req.body)
+      .then((updatedRole) => {
+        res.status(200)
+          .send({
+            message: 'This role has been updated',
+            updatedRole
+          });
+      })
+      .catch(error =>
+        res.status(400)
+          .send({
+            errorArray: Helper.errorArray(error)
+          }));
+  },
+
+  /**
+    * Delete a Role
+    * Route: DELETE: /roles/:id
+    * @param {Object} req request object
+    * @param {Object} res response object
+    * @returns {void} no returns
+    */
+  deleteRole(req, res) {
+    console.log(res, 'andelaaaa');
+    req.roleInstance.destroy()
+      .then((updatedRole) => {
+        res.status(200)
+          .send({
+            message: 'This role has been deleted',
+            updatedRole
+          });
+      });
+  },
+  /**
+   * retrieve -  return a role
+   * @param {Object}  request request object
+   * @param {Object}  response response object
+   * @returns {void} - returns void
+   */
+  getRoleById(req, res) {
+    db.Roles.findById(req.params.id)
+      .then((role) => {
+        if (!role) {
+          return res.status(404).send({
+            message: 'Role does not exists'
+          });
+        }
+        return res.status(200).send({
+          role
+        });
+      });
+  }
 };
 
 export default RoleController;
