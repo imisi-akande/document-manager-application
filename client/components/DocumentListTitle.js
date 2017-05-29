6906f295883e44a419bf7deef563e3b57dd71923
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-05-30 00:28:30 +0100

    Modify client folder

--- client/components/DocumentListTitle.js
+++
@@ -1,9 +0,0 @@
-import React, { propTypes } from 'react';
-
-const DocumentTitle = ({ title }) => {
-    return (
-        <span className="card-title">{title}</span>
-    );
-};
-
-export default DocumentTitle;
