a85dedd6d77ca4e0f8e6d3bdad7b7962062effd3
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-06-17 18:44:12 +0100

    delete console

--- client/components/document/OwnDocumentList.jsx
+++ client/components/document/OwnDocumentList.jsx
@@ -45,7 +45,6 @@ class OwnDocumentList extends React.Component {
    * @memberOf OwnDocumentList
    */
   componentDidMount() {
-    console.log('am here');
     this.props.fetchOwnDocuments();
   }
 
